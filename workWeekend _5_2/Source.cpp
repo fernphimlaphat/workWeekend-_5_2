d43d5574af698191f867fe80a420ab4fe8aca24f
Phimlaphat Surayaem <fernphimlaphat_44@hotmail.com>
2020-09-12 01:03:29 +0700

    add lose/get

--- workWeekend _5_2/Source.cpp
+++ workWeekend _5_2/Source.cpp
@@ -11,14 +11,14 @@ void random1() {
 
 void random3() {
 
-	printf("\n[2] Random 2\n");
+	printf("\n[3] Random 2\n");
 	printf("\n1.Apple\n2.Orange\n3.Grape\n4.Strawberry\n\n");
 }
 
 
 void random2() {
 
-	printf("\n[3]Random 3\n");
+	printf("\n[2]Random 3\n");
 	printf("\n1.Diamond\n2.Watch\n3.Bracelet\n4.Ring\n");
 }
 
@@ -35,21 +35,21 @@ void show1(int coin) {
 
 	char com = random1_1();
 
-	if (com == '1') { printf("Congrat!\n"); printf("Car\n"); printf("You have coin %d", coin); }
+	if (com == '1') {printf("Car\n"); printf("You have coin %d", coin); }
 
-	else if (com == '2') { printf("Congrat!\n"); printf("Fan\n");  printf("You have coin %d", coin); }
+	else if (com == '2') { printf("Fan\n");  printf("You have coin %d", coin); }
 
-	else if (com == '3') { printf("Congrat!\n"); printf("Mobile\n"); printf("You have coin %d", coin); }
+	else if (com == '3') { printf("Mobile\n"); printf("You have coin %d", coin); }
 
-	else if (com == '4') { printf("Congrat!\n"); printf("Mirror\n"); printf("You have coin %d", coin); }
+	else if (com == '4') { printf("Mirror\n"); printf("You have coin %d", coin); }
 
 	else
 	{
 
 		if (rand() % 3 == 1)
 		{
-			printf("add %d coin\n", 500);
-			coin = coin + 500;
+			printf("add %d coin\n", 1000);
+			coin = coin + 1000;
 			printf("You have coin %d", coin);
 		}
 		else if (rand() % 3 == 2)
@@ -77,20 +77,20 @@ void show2(int coin) {
 
 	char com = random1_1();
 
-	if (com == '1') { printf("Congrat!\n"); printf("Diamond\n"); printf("You have coin %d", coin); }
+	if (com == '1') {  printf("Diamond\n");  printf("You have coin %d", coin ); }
 
-	else if (com == '2') { printf("Congrat!\n"); printf("Watch\n"); printf("You have coin %d", coin); }
+	else if (com == '2') { printf("Watch\n");  printf("You have coin %d", coin ); }
 
-	else if (com == '3') { printf("Congrat!\n"); printf("Bracelet\n"); printf("You have coin %d", coin); }
+	else if (com == '3') { printf("Bracelet\n"); printf("You have coin %d", coin); }
 
-	else if (com == '4') { printf("Congrat!\n"); printf("Ring\n"); printf("You have coin %d", coin); }
+	else if (com == '4') { printf("Ring\n"); printf("You have coin %d", coin); }
 
 	else
 	{
 		if (rand() % 3 == 1)
 		{
-			printf("add %d coin\n", 500);
-			coin = coin + 500;
+			printf("add %d coin\n", 1000);
+			coin = coin + 1000;
 			printf("You have coin %d", coin);
 		}
 		else if (rand() % 3 == 2)
@@ -115,20 +115,20 @@ void show3(int coin) {
 
 	char com = random1_1();
 
-	if (com == '1') { printf("Congrat!\n"); printf("Apple"); printf("You have coin %d", coin); }
+	if (com == '1') {  printf("Apple \n"); printf("You have coin %d\n", coin); }
 
-	else if (com == '2') { printf("Congrat!\n"); printf("Orange"); printf("You have coin %d", coin); }
+	else if (com == '2') {  printf("Orange\n"); printf("You have coin %d\n", coin); }
 
-	else if (com == '3') { printf("Congrat!\n"); printf("Grape"); printf("You have coin %d", coin); }
+	else if (com == '3') { printf("Grape\n"); printf("You have coin %d\n", coin); }
 
-	else if (com == '4') { printf("Congrat!\n"); printf("Strawberry"); printf("You have coin %d", coin); }
+	else if (com == '4') { printf("Strawberry\n"); printf("You have coin %d\n", coin); }
 
 	else
 	{
 		if (rand() % 3 == 1)
 		{
-			printf("add %d coin\n", 500);
-			coin = coin + 500;
+			printf("add %d coin\n", 1000);
+			coin = coin + 1000;
 			printf("You have coin %d", coin);
 		}
 		else if (rand() % 3 == 2)
@@ -146,12 +146,13 @@ void show3(int coin) {
 		}
 	}
 
-
 }
 
 int main() {
+	srand(time(NULL));
 
-	int coin = 2000;		
+
+	int coin = 1000;		
 	int v;
 	printf("You have %d coin\n",coin);
 	random1();
@@ -168,7 +169,15 @@ int main() {
 		if (v == 1)
 		{
 			coin = coin - 500;
-			show1(coin);
+			show1(coin); if ( rand() % 2 == 0)
+			{
+				printf("\nyou get it\n");
+			}
+			else
+			{
+				printf("\nYou lose\n");
+			}
+			
 
 		}
 
@@ -176,12 +185,29 @@ int main() {
 		{
 			coin = coin - 500;
 			show2(coin);
+			if (rand() % 2 == 0)
+			{
+				printf("\nyou get it\n");
+			}
+			else
+			{
+				printf("\nYou lose\n");
+			}
+
 		}
 
 		else if (v==3)
 		{
 			coin = coin - 500;
 			show3(coin);
+			if (rand() % 2 == 0)
+			{
+				printf("\nyou get it\n");
+			}
+			else
+			{
+				printf("\nYou lose\n");
+			}
 		}
 
 		else if (v==4)
@@ -199,4 +225,7 @@ int main() {
 
 
 	} while (coin >= 500 );
+
+
+
 }
